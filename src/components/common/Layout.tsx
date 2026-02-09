b1eb6442cd5107eaba8123a2e5109e31853ebcf7
molevent <poii@molevent.tech>
2026-02-09 09:46:03 +0700

    Change sidebar background to soft mint green #D4E8D4

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -124,7 +124,7 @@ export default function Layout({ children }: LayoutProps) {
       )}
       
       {/* Sidebar */}
-      <div className={`fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out ${
+      <div className={`fixed inset-y-0 left-0 z-50 w-64 bg-[#D4E8D4] shadow-lg transform transition-transform duration-300 ease-in-out ${
         sidebarOpen ? 'translate-x-0' : '-translate-x-full'
       } lg:translate-x-0`}>
         <div className="flex flex-col h-full">
